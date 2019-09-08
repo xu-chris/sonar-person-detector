1d2ff4a2291f4b54d13a1b9f477feba990c93330
Christian Reichel <chrstn.rchl@gmail.com>
2019-09-08 15:27:03 +0200

    🎉 Add processing and opening in webbrowser

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from flask import Flask  # For REST API suppprt
 from flask import jsonify
 from flask import render_template
 from flask_cors import CORS
+from multiprocessing import Process
+import socket
+import webbrowser
 
 # Setup player server
 app = Flask(__name__, template_folder='static')
@@ -46,10 +49,27 @@ class BucketPlayer:
         self.bucket_state = new_bucket_state
 
 
+class Server(Thread):
+
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            # Get the work from the queue and expand the tuple
+            host, port, debug = self.queue.get()
+            try:
+                app.run(host=host, port=port, debug=debug)
+            finally:
+                self.queue.task_done()
+
+
 def set_logging():
-    logging.basicConfig(filename='BucketPlayer.log', level=logging.DEBUG)
-    logging.basicConfig(format='%(asctime)s %(message)s')
-    logging.info('Started service')
+    # logging.basicConfig(filename='BucketPlayer.log', level=print)
+    # logging.basicConfig(format='%(asctime)s %(message)s')
+    # logging.info('Started service. Logging level {}'.format(config['logging_level']))
+    pass
 
 
 def read_config():
@@ -61,6 +81,52 @@ def read_config():
 
 def run_detection():
     global bucketplayer
+
+    while True:
+        bucketplayer.run_detection()
+        time.sleep(0.1)
+
+
+def run_server(host='0.0.0.0', port=5000, debug=False, threading=True):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    print('Starting server on http://{}:{}'.format(s.getsockname()[0],port))
+    app.run(host=host, port=port, debug=debug, threaded=threading)
+
+
+def run_browser():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    url = 'http://{}:5000'.format(s.getsockname()[0])
+    webbrowser.open_new_tab(url)
+
+
+def run_in_parallel(*fns):
+    proc = []
+    for fn in fns:
+        p = Process(target=fn)
+        p.start()
+        proc.append(p)
+    for p in proc:
+        p.join()
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route('/current/', methods=['GET'])
+def get_player_state():
+    global bucketplayer
+    print('Frontend fetches current state...')
+    state = bucketplayer.player.state()
+    return jsonify({'state': state}), 201
+
+
+if __name__ == '__main__':
+    read_config()
+    set_logging()
 
     devices = [
         [
@@ -83,28 +149,4 @@ def run_detection():
 
     bucketplayer = BucketPlayer(devices)
 
-    while True:
-        bucketplayer.run_detection()
-        time.sleep(0.1)
-
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-
-@app.route('/current/', methods=['GET'])
-def get_player_state():
-    print('Frontend fetches current state...')
-    state = bucketplayer.player.state()
-    return jsonify({'state': state}), 201
-
-
-if __name__ == '__main__':
-
-    read_config()
-
-    set_logging()
-
-    server = Thread(target=app.run(host='0.0.0.0', port=5000, debug=True)).start()
-    detector = Thread(run_detection()).start()
+    run_in_parallel(run_detection, run_server, run_browser)
